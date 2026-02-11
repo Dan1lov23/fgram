6d05c602379c3e04572fbb627e97b381521f7e5d
kitik <svy1dan@gmail.com>
2026-02-11 12:49:29 +0300

    Add check account confirm check

--- src/page components/Confirm account/page/ConfirmAccount.tsx
+++ src/page components/Confirm account/page/ConfirmAccount.tsx
@@ -1,26 +1,52 @@
 import "./confirmAccount.css";
 
+import { useEffect, useState } from "react";
+
 import { useParams, Navigate } from "react-router-dom";
 
 import ConfirmAccountTitle from "../components/confirm account title/ConfirmAccountTitle.tsx";
+import ConfirmAccountWarning from "../../../UI components/account is confirm warning/ConfirmAccountWarning.tsx";
 import ConfirmAccountButton from "../components/confirm account button/ConfirmAccountButton.tsx";
+
+import { checkActivatedFunction } from "../../../API/auth API functions/check activated function/checkActivatedFunction.ts";
 
 export default function ConfirmAccount() {
 
     const { username } = useParams();
 
-    console.log(localStorage.getItem("username"), username);
+    const [checkActivated, setCheckActivated] = useState(false);
+
+    useEffect(() => {
+
+        const checkActivated = async() => {
+
+            const check = await checkActivatedFunction();
+
+            if (check) {
+                setCheckActivated(true)
+            }
+        }
+
+        checkActivated();
+
+    })
 
     return (
         <>
             <div className="confirmAccount">
-                {localStorage.getItem("username") === username ? (
+                {checkActivated ? (
                     <div>
-                        <ConfirmAccountTitle/>
-                        <ConfirmAccountButton/>
+                        {localStorage.getItem("username") === username ? (
+                            <div>
+                                <ConfirmAccountTitle/>
+                                <ConfirmAccountButton/>
+                            </div>
+                        ) : (
+                            <Navigate to="/*"/>
+                        )}
                     </div>
                 ) : (
-                    <Navigate to="/*"/>
+                    <ConfirmAccountWarning/>
                 )}
             </div>
         </>
